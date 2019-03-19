277f295fbdfc1e7db1c0f7fc2d96c11ab1cd2505
yanpingyuan <yanpingyuan@shinetechsoftware.com>
2019-03-19 17:37:49 +0800

    hide image entry image

--- TestTab.Android/Renderer/ImageEntryRenderer.cs
+++ TestTab.Android/Renderer/ImageEntryRenderer.cs
@@ -33,30 +33,30 @@ namespace TestTab.Droid.Renderer
 
 
             var editText = this.Control;
-            if (!string.IsNullOrEmpty(element.Image))
-            {
-                switch (element.ImageAlignment)
-                {
-                    case ImageAlignment.Left:
-                        editText.SetCompoundDrawablesWithIntrinsicBounds(GetDrawable(element.Image), null, null, null);
-                        break;
-                    case ImageAlignment.Right:
-                        editText.SetCompoundDrawablesWithIntrinsicBounds(null, null, GetDrawable(element.Image), null);
-                        break;
-                }
-            }
-            editText.CompoundDrawablePadding = 25;
+            //if (!string.IsNullOrEmpty(element.Image))
+            //{
+            //    switch (element.ImageAlignment)
+            //    {
+            //        case ImageAlignment.Left:
+            //            editText.SetCompoundDrawablesWithIntrinsicBounds(GetDrawable(element.Image), null, null, null);
+            //            break;
+            //        case ImageAlignment.Right:
+            //            editText.SetCompoundDrawablesWithIntrinsicBounds(null, null, GetDrawable(element.Image), null);
+            //            break;
+            //    }
+            //}
+            //editText.CompoundDrawablePadding = 25;
             Control.Background.SetColorFilter(element.LineColor.ToAndroid(), PorterDuff.Mode.SrcAtop);
         }
 
-        private BitmapDrawable GetDrawable(string imageEntryImage)
-        {
-            int resID = Resources.GetIdentifier(imageEntryImage, "drawable", this.Context.PackageName);
-            var drawable = ContextCompat.GetDrawable(this.Context, resID);
-            var bitmap = ((BitmapDrawable)drawable).Bitmap;
+        //private BitmapDrawable GetDrawable(string imageEntryImage)
+        //{
+        //    int resID = Resources.GetIdentifier(imageEntryImage, "drawable", this.Context.PackageName);
+        //    var drawable = ContextCompat.GetDrawable(this.Context, resID);
+        //    var bitmap = ((BitmapDrawable)drawable).Bitmap;
 
-            return new BitmapDrawable(Resources, Bitmap.CreateScaledBitmap(bitmap, element.ImageWidth * 2, element.ImageHeight * 2, true));
-        }
+        //    return new BitmapDrawable(Resources, Bitmap.CreateScaledBitmap(bitmap, element.ImageWidth * 2, element.ImageHeight * 2, true));
+        //}
 
     }
 }
--- TestTab.iOS/Renderers/ImageEntryRenderer.cs
+++ TestTab.iOS/Renderers/ImageEntryRenderer.cs
@@ -22,20 +22,20 @@ namespace TestTab.iOS.Renderers
 
             var element = (ImageEntry)this.Element;
             var textField = this.Control;
-            if (!string.IsNullOrEmpty(element.Image))
-            {
-                switch (element.ImageAlignment)
-                {
-                    case ImageAlignment.Left:
-                        textField.LeftViewMode = UITextFieldViewMode.Always;
-                        textField.LeftView = GetImageView(element.Image, element.ImageHeight, element.ImageWidth);
-                        break;
-                    case ImageAlignment.Right:
-                        textField.RightViewMode = UITextFieldViewMode.Always;
-                        textField.RightView = GetImageView(element.Image, element.ImageHeight, element.ImageWidth);
-                        break;
-                }
-            }
+            //if (!string.IsNullOrEmpty(element.Image))
+            //{
+            //    switch (element.ImageAlignment)
+            //    {
+            //        case ImageAlignment.Left:
+            //            textField.LeftViewMode = UITextFieldViewMode.Always;
+            //            textField.LeftView = GetImageView(element.Image, element.ImageHeight, element.ImageWidth);
+            //            break;
+            //        case ImageAlignment.Right:
+            //            textField.RightViewMode = UITextFieldViewMode.Always;
+            //            textField.RightView = GetImageView(element.Image, element.ImageHeight, element.ImageWidth);
+            //            break;
+            //    }
+            //}
 
             textField.BorderStyle = UITextBorderStyle.None;
             CALayer bottomBorder = new CALayer
@@ -49,16 +49,16 @@ namespace TestTab.iOS.Renderers
             textField.Layer.MasksToBounds = true;
         }
 
-        private UIView GetImageView(string imagePath, int height, int width)
-        {
-            var uiImageView = new UIImageView(UIImage.FromBundle(imagePath))
-            {
-                Frame = new RectangleF(0, 0, width, height)
-            };
-            UIView objLeftView = new UIView(new System.Drawing.Rectangle(0, 0, width + 10, height));
-            objLeftView.AddSubview(uiImageView);
+        //private UIView GetImageView(string imagePath, int height, int width)
+        //{
+        //    var uiImageView = new UIImageView(UIImage.FromBundle(imagePath))
+        //    {
+        //        Frame = new RectangleF(0, 0, width, height)
+        //    };
+        //    UIView objLeftView = new UIView(new System.Drawing.Rectangle(0, 0, width + 10, height));
+        //    objLeftView.AddSubview(uiImageView);
 
-            return objLeftView;
-        }
+        //    return objLeftView;
+        //}
     }
 }
