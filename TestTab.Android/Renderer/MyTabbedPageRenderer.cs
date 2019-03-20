40622d9b7484ab2fd215328f82d951a839f7db95
yanpingyuan <yanpingyuan@shinetechsoftware.com>
2019-03-20 10:41:39 +0800

    change tabbed bar icon to center vertically

---
+++ TestTab.Android/Renderer/MyTabbedPageRenderer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Android.Content;
+using Android.Support.Design.Internal;
+using Android.Views;
+using Android.Widget;
+using TestTab.Droid.Renderer;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using Xamarin.Forms.Platform.Android.AppCompat;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(MyTabbedPageRenderer))]
+namespace TestTab.Droid.Renderer
+{
+    public class MyTabbedPageRenderer : TabbedPageRenderer
+    {
+        public MyTabbedPageRenderer(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<TabbedPage> e)
+        {
+            base.OnElementChanged(e);
+
+            var childViews = GetAllChildViews(ViewGroup);
+
+            var scale = Resources.DisplayMetrics.Density;
+            var paddingDp = 9;
+            var dpAsPixels = (int)(paddingDp * scale + 0.5f);
+
+            foreach (var childView in childViews)
+            {
+                if (childView is BottomNavigationItemView tab)
+                {
+                    tab.SetPadding(tab.PaddingLeft, dpAsPixels, tab.PaddingRight, tab.PaddingBottom);
+                }
+                else if (childView is TextView textView)
+                {
+                    textView.SetTextColor(Android.Graphics.Color.Transparent);
+                }
+            }
+        }
+
+        List<Android.Views.View> GetAllChildViews(Android.Views.View view)
+        {
+            if (!(view is ViewGroup group))
+            {
+                return new List<Android.Views.View> { view };
+            }
+
+            var result = new List<Android.Views.View>();
+
+            for (int i = 0; i < group.ChildCount; i++)
+            {
+                var child = group.GetChildAt(i);
+
+                var childList = new List<Android.Views.View> { child };
+                childList.AddRange(GetAllChildViews(child));
+
+                result.AddRange(childList);
+            }
+
+            return result.Distinct().ToList();
+        }
+    }
+}
---
+++ TestTab.iOS/Renderers/MyTabbedPageRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using TestTab.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using Xamarin.Forms.PlatformConfiguration;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(MyTabbedPageRenderer))]
+namespace TestTab.iOS.Renderers
+{
+    public class MyTabbedPageRenderer : TabbedRenderer
+    {
+
+        public override void ViewWillLayoutSubviews()
+        {
+            base.ViewWillLayoutSubviews();
+
+            if (TabBar != null)
+            {
+                foreach (var item in TabBar.Items)
+                {
+                    item.ImageInsets = new UIEdgeInsets(5, 0, -5, 0);
+                }
+            }
+        }
+
+    }
+}
